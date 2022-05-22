86a7b8ca3c88ff7b17d75046c7ff5b2a111e97ec
m4st3rm1ndtm <m4st3rm1nd.tm@gmail.com>
2022-05-22 23:56:03 +0300

    1 file modified, 2 added

---
+++ src/app/harmonics-player/harmonics-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-harmonics-player',
+  templateUrl: './harmonics-player.component.html',
+  styleUrls: ['./harmonics-player.component.css']
+})
+export class HarmonicsPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
